2940ba16c178a0c53f85ab3d0cad78acdc6f063b
LeeTaeHwa <driversivese@gmail.com>
2023-03-07 14:32:05 +0900

    READ ME scene

--- Assets/Script/ReadMe.cs
+++ Assets/Script/ReadMe.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ReadMe : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private string description;
+    [SerializeField] private GUIStyle guiStyle;
+
+    private void OnGUI()
     {
-        
+        GUI.Box(new Rect(180, 180, 1450F, 800f), string.Empty);
+
+        GUI.Label(new Rect(200, 200, 1400f, 100f), "Read Me", guiStyle);
+
+        var smallStyle = new GUIStyle();
+        smallStyle.fontSize = 28;
+    
+        GUI.Label (new Rect(200, 350, 1400f, 700f), description, smallStyle);
     }
 }
